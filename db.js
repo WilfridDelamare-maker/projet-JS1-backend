2aafc1eb3a02c213455312ebf6193172e1e37f9c
Wilfrid <wilfrid.delamare@ynov.com>
2026-03-18 16:25:02 +0100

    db

---
+++ db.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+// chemin vers la bdd
+const dbPath = path.join(__dirname, 'database/project.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Erreur connexion SQLite :', err.message);
+  } else {
+    console.log('Connecté à SQLite');
+    db.run('PRAGMA foreign_keys = ON');
+  }
+});
+
+module.exports = db;
